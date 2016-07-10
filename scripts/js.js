a2c5fee08cfbd84ec101a0624dd242263bbecf35
derelektrischemoench <baederchris@gmail.com>
2016-07-10 15:26:29 +0200

    toggle works

--- scripts/js.js
+++ scripts/js.js
@@ -2,3 +2,9 @@
 /*$(document).ready(function () {
     alert("asdads");
 });*/
+
+$(document).ready(function(){
+    $('.button1').click(function () {
+        $('.box1').toggleClass('blue');
+    })
+});
