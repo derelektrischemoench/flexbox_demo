2c2d71019ec4fca7f84337258646b74727127155
derelektrischemoench <baederchris@gmail.com>
2016-07-10 16:03:33 +0200

    looking good so far....

--- scripts/js.js
+++ scripts/js.js
@@ -6,6 +6,17 @@
 $(document).ready(function(){
     $('.button1').click(function () {
         $('#row_wrapper').toggleClass('isFlexRow');
+
+        if($('#row_wrapper').hasClass('isFlexRow')){
+            $('.code p').empty();
+            $('.code p').append("#row_wrapper {display:flex}");
+        }
+
+        else {
+            $('.code p').empty();
+            $('.code p').append("#row_wrapper {display:block}");
+        }
+
     });
 
     $('.button2').click(function () {
