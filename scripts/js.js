23cf307c650869cddbde350300b1a72fe8a7aa1b
derelektrischemoench <baederchris@gmail.com>
2016-07-10 18:56:21 +0200

    toggle plus labels works okay now

--- scripts/js.js
+++ scripts/js.js
@@ -1,38 +1,33 @@
-
-/*$(document).ready(function () {
-    alert("asdads");
-});*/
-
 $(document).ready(function(){
-   $('.code p').prepend("#row_wrapper:{display:block}");
 
+    var rwdb = "#row_wrapper:{display:block}";
+    var cwdb = "#column_wrapper:{display:block}";
+    var rwdf = "#row_wrapper:{display:flex}";
+
+
+   $('.code p').prepend("<p>" + rwdb + "<br>" + cwdb);
 
     $('.button1').click(function () {
         $('.code p').remove();
         $('#row_wrapper').toggleClass('isFlexRow');
 
         if($('#row_wrapper').hasClass('isFlexRow')){
-            $('.code ').prepend("<p>#row_wrapper:{display:flex}</p>");
+            $('.code ').prepend("<p>" + rwdf + "<br>" + cwdb + "</p>");
         }
 
-        else ($('.code').prepend("<p>#row_wrapper:{display:block}</p>"));
-
-
-        
-        /*if($('#row_wrapper').hasClass('isFlexRow')){
-            $('.code p').empty();
-            $('.code p').append("#row_wrapper {display:flex}");
-        }
-
-        else {
-            $('.code p').empty();
-            $('.code p').append("#row_wrapper {display:block}");
-        }*/
-
+        else ($('.code').prepend("<p>" + rwdb + "<br>" + cwdb));
     });
 
     $('.button2').click(function () {
-        $('#column_wrapper').toggleClass('isFlexColumn');
-        /*$('.code p').append("#row_wrapper {display:flex}")*/
+
+        if($('#row_wrapper').hasClass('isFlexRow')){
+            //do stuff
+            $('#column_wrapper').toggleClass('isFlexColumn');
+        }
+
+        else{
+            //do nothing
+        }
+
     });
 });
