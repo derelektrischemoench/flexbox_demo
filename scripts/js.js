f92d28ae75e3d1d5551d89d7240394ee0b9ff02d
derelektrischemoench <baederchris@gmail.com>
2016-07-10 17:01:46 +0200

    now we know how to insert text into the dom

--- scripts/js.js
+++ scripts/js.js
@@ -4,10 +4,12 @@
 });*/
 
 $(document).ready(function(){
+    $("<p>asdasd</p>").insertAfter($('.code span'));
+
     $('.button1').click(function () {
         $('#row_wrapper').toggleClass('isFlexRow');
-
-        if($('#row_wrapper').hasClass('isFlexRow')){
+        
+        /*if($('#row_wrapper').hasClass('isFlexRow')){
             $('.code p').empty();
             $('.code p').append("#row_wrapper {display:flex}");
         }
@@ -15,11 +17,12 @@ $(document).ready(function(){
         else {
             $('.code p').empty();
             $('.code p').append("#row_wrapper {display:block}");
-        }
+        }*/
 
     });
 
     $('.button2').click(function () {
         $('#column_wrapper').toggleClass('isFlexColumn');
+        /*$('.code p').append("#row_wrapper {display:flex}")*/
     });
 });
