8476d863602c4b4c401afc840172e63271a7a33b
Chris Bader <baederchris@gmail.com>
2016-07-11 07:56:44 +0200

    fix this

--- scripts/js.js
+++ scripts/js.js
@@ -17,20 +17,24 @@ $(document).ready(function(){
             $('.code ').prepend("<p>" + rwdf + "<br>" + cwdb + "</p>");
         }
 
-        else ($('.code').prepend("<p>" + rwdb + "<br>" + cwdb));
+        else ($('.code').prepend("<p>" + rwdb + "<br>" + cwdb + "</p>"));
     });
 
     $('.button2').click(function () {
 
         if($('#row_wrapper').hasClass('isFlexRow')){
             //empty the code box
-            $('.code p').remove();
+            //$('.code p').remove();TODO: fix this, this emptys the box all the time
             //do stuff aka only enable button two if container is flex
             $('#column_wrapper').toggleClass('isFlexColumn');
-            $('.code').prepend("<p>" + rwdf + "<br>" + cwdf + "</br>");
+            $('.code p').prepend("<p>" + rwdf + "<br>" + cwdf + "</p>");
 
             if($('#column_wrapper').hasClass('is FlexColumn')){
-                $('.code').prepend("<p>" + rwdf + "<br>" + cwdf);
+                $('.code').prepend("<p>" + rwdf + "<br>" + cwdf + "</p>");
+            }
+            else {
+                //$('.code p').remove();
+                //$('.code').prepend("<p>" + rwdf + "<br>" + cwdb + "</p>");
             }
         }
 
