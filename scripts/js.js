0eab5b795cd274cbf47b357bec7a9eb59553304f
Chris Bader <baederchris@gmail.com>
2016-07-11 09:13:59 +0200

    aaaaaA

--- scripts/js.js
+++ scripts/js.js
@@ -14,15 +14,22 @@ $(document).ready(function(){
         $('.button1').toggleClass('flextoggle_active');
         $('#row_wrapper').toggleClass('isFlexRow');
 
+        if($('.button1').hasClass('active') && $('.button2').hasClass('flextoggle_active')){
+            $('.button1').click(function () {
+                $('#column_wrapper').removeClass('flextoggle_active'),
+                    $('#row_wrapper').removeClass('flextoggle_active');
+            });
+        }
+
         if($('#row_wrapper').hasClass('isFlexRow')){
             $('.code ').prepend("<p>" + rwdf + "<br>" + cwdb + "</p>");
         }
 
         else ($('.code').prepend("<p>" + rwdb + "<br>" + cwdb + "</p>"));
+
     });
 
     $('.button2').click(function () {
-
         if($('#row_wrapper').hasClass('isFlexRow')){
             $('.code p').remove();
             $('#column_wrapper').toggleClass('isFlexColumn');
