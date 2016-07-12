cc9261a52aec283761ad97b3ef02caf33d6caaa7
derelektrischemoench <baederchris@gmail.com>
2016-07-12 19:11:04 +0200

    lel

--- scripts/js_page3.js
+++ scripts/js_page3.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     var display = $('#column_wrapper3').css('display');
-    
+
 
     $('.code p').prepend( "#column_wrapper{display:" + display +"}<br>" );
     $('.code p').append( "#column_wrapper{justify-content:flex-start}");
@@ -10,56 +10,58 @@ $(document).ready(function(){
         $('#column_wrapper3').prepend("<div class='box1'></div>");
     });
 
-    toggleJustifyContent=function () {
+    var toggleJustifyContent=function () {
 
-        if($('.legende').hasClass('stage1')){
-            $('#column_wrapper3').addClass('jcsa');
-            $('.code p').remove();
-            $('.code').append(
-                "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>" +
-                "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
-            );
-            $('.legende').removeClass('stage1');
-            $('.legende').addClass('stage2');
-        }
+            if ($('.legende').hasClass('stage1')) {
+                $('#column_wrapper3').addClass('jcsa');
+                $('.code p').remove();
+                $('.code').append(
+                    "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>" +
+                    "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
+                );
+                $('.legende').removeClass('stage1');
+                $('.legende').addClass('stage2');
+            }
 
-        else if ( $('.legende').hasClass('stage2') ) {
+            else if ($('.legende').hasClass('stage2')) {
 
-            $('#column_wrapper3').removeClass('jcsa');
-            $('#column_wrapper3').addClass('jcsb');
-            $('.code p').remove();
-            $('.code').append(
-                "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>" +
-                "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
-            );
-            $('.legende').removeClass('stage2');
-            $('.legende').addClass('stage3');
-        }
+                $('#column_wrapper3').removeClass('jcsa');
+                $('#column_wrapper3').addClass('jcsb');
+                $('.code p').remove();
+                $('.code').append(
+                    "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>" +
+                    "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
+                );
+                $('.legende').removeClass('stage2');
+                $('.legende').addClass('stage3');
+            }
 
-        //adn now for justify-content center
-        else if ( $('.legende').hasClass('stage3') ){
-            $('#column_wrapper3').removeClass('jcsb');
-            $('#column_wrapper3').addClass('jcsc');
-            $('.code p').remove();
-            $('.code').append(
-                "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>" +
-                "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
-            );
-            $('.legende').removeClass('stage3');
-            $('.legende').addClass('stage4');
-        }
+            //adn now for justify-content center
+            else if ($('.legende').hasClass('stage3')) {
+                $('#column_wrapper3').removeClass('jcsb');
+                $('#column_wrapper3').addClass('jcsc');
+                $('.code p').remove();
+                $('.code').append(
+                    "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>" +
+                    "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
+                );
+                $('.legende').removeClass('stage3');
+                $('.legende').addClass('stage4');
+            }
 
-        else if ( $('.legende').hasClass('stage4') ){
-            $('#column_wrapper3').removeClass('jcsc');
-            $('#column_wrapper3').addClass('jcfe');
-            $('.code p').remove();
-            $('.code').append(
-                "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>" +
-                "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
-            );
-            $('.legende').removeClass('stage4');
-            $('.legende').addClass('stage5');
-        }
+            else if ($('.legende').hasClass('stage4')) {
+                $('#column_wrapper3').removeClass('jcsc');
+                $('#column_wrapper3').addClass('jcfe');
+                $('.code p').remove();
+                $('.code').append(
+                    "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>" +
+                    "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
+                );
+                $('.legende').removeClass('stage4');
+                $('.legende').addClass('stage5');
+            }
+
+
 
     };
 
