613cc8d5282010cfccd262bace22e390788df67b
derelektrischemoench <baederchris@gmail.com>
2016-07-12 18:35:13 +0200

    fuck yissss

--- scripts/js_page3.js
+++ scripts/js_page3.js
@@ -12,16 +12,27 @@ $(document).ready(function(){
 
     toggleJustifyContent=function () {
 
+        if(!$('.legende').hasClass('moep')){
             $('#column_wrapper3').addClass('jcsa');
             $('.code p').remove();
             $('.code').append(
-                "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>"+
-                    "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
+                "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>" +
+                "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
             );
+            $('.legende').addClass('moep');
 
-            $('.button2').click(function () {
-                $('#column_wrapper3').removeClass('jcsa');
-            });
+            console.log('function called');
+        }
+
+        else {
+            $('#column_wrapper3').removeClass('jcsa');
+            $('#column_wrapper3').addClass('jcsb');
+            $('.code p').remove();
+            $('.code').append(
+                "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>" +
+                "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
+            );
+        }
 
     };
 
