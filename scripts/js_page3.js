d80a8910d82d225efdc8935e5bb482d05992adbf
derelektrischemoench <baederchris@gmail.com>
2016-07-12 18:19:34 +0200

    outsourced the clickstuff into a dedicated function

--- scripts/js_page3.js
+++ scripts/js_page3.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
 
-    $('.code p').prepend( "#column_wrapper{display:" + $('#column_wrapper3').css('display') +"}<br>" );
-    $('.code p').append( "#column_wrapper{justify-content:" + $('#column_wrapper3').css('justify-content') + "}");
+    var display = $('#column_wrapper3').css('display');
+    var justifyContent = $('#column_wrapper3').css('justify-content');
+
+    $('.code p').prepend( "#column_wrapper{display:" + display +"}<br>" );
+    $('.code p').append( "#column_wrapper{justify-content:" + justifyContent + "}");
 
     $('.button1').click(function () {
         $('#column_wrapper3').prepend("<div class='box1'></div>");
     });
 
+    toggleJustifyContent=function () {
+
+            $('#column_wrapper3').addClass('jcsa');
+            $('.code p').remove();
+            $('.code').append(
+                "<p> #column-wrapper {display:" + $("#column_wrapper3").css("display") + "}<br>"+
+                    "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}</p>"
+            );
+
+            $('.button2').click(function () {
+                $('#column_wrapper3').removeClass('jcsa');
+            });
+
+    };
+
     $('.button2').click(function () {
-        $('#column_wrapper3').css('justify-content', 'space-around');
-        $('.code p').remove();
-        $('.code').append(
-            "<p>#column_wrapper{display:" + $('#column_wrapper3').css('display') +"}<br>" +
-            "#column-wrapper {justify-content:" + $("#column_wrapper3").css("justify-content") + "}<br>"+"</p>"
-        );
+        toggleJustifyContent();
     });
 });
