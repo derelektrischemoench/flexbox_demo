f02ea4ce703a18c4d20bc428c01fb1a7ffad2390
derelektrischemoench <baederchris@gmail.com>
2016-07-18 20:25:44 +0200

    page3 justify content upgraded to switches

--- scripts/js_page3.js
+++ scripts/js_page3.js
@@ -1,23 +1,53 @@
-//also todo: fix the display
-
 $(document).ready(function(){
     var cwDisplayState = $('#column_wrapper4').css('display');
     var rwDisplayState =  $('#row_wrapper4').css('display');
     var cwFlexState = $('#column_wrapper4').css('justify-content');
     var rwFlexState = $('#row_wrapper4').css('justify-content');
     var display = $('#column_wrapper4').css('display');
+    var columnWrapperState = 0;
 
 
 
     $(window).ready(function () {
+        toggleJustifyContent();
         updateDisplay();
     });
 
-
-
     var toggleJustifyContent=function () {
 
-            if ($('#column_wrapper4').hasClass('jcc')) {
+        switch(columnWrapperState){
+            //flex-start
+            case 0: $('#column_wrapper4').removeClass();
+                    $('#column_wrapper4').addClass('jcfs');
+                    columnWrapperState = 1;
+                break;
+            
+            //center
+            case 1: $('#column_wrapper4').removeClass();
+                    $('#column_wrapper4').addClass('jcc');
+                    columnWrapperState = 2;
+                break;
+
+            //flex-end
+            case 2: $('#column_wrapper4').removeClass();
+                $('#column_wrapper4').addClass('jcfe');
+                columnWrapperState = 3;
+                break;
+
+            //space-around
+            case 3: $('#column_wrapper4').removeClass();
+                $('#column_wrapper4').addClass('jcsb');
+                columnWrapperState = 4;
+                break;
+
+            //space-between
+            case 4: $('#column_wrapper4').removeClass();
+                $('#column_wrapper4').addClass('jcsa');
+                columnWrapperState = 0;
+                break;
+        }
+
+            /*if ($('#column_wrapper4').hasClass('jcc')) {
 
                 $('#column_wrapper4').addClass('jcsa');
                 $('#column_wrapper4').removeClass('jcc');
@@ -49,7 +79,7 @@ $(document).ready(function(){
                 $('.legende').removeClass('stage4');
                 $('.legende').addClass('stage1');
                 updateDisplay();
-            }
+            }*/
 
 
 
@@ -66,23 +96,25 @@ $(document).ready(function(){
 
     var addBlock = function () {
         $('#column_wrapper4').prepend("<div class='box1'></div>");
+        updateDisplay();
     };
 
     var getFlexState = function (element) {
         return( $(element).css('justify-content') );
     };
 
-    $('.button2').click(function () {
-        toggleJustifyContent();
-    });
-
-    //add a block to the container
+    //add a block to the container and update the display
     $('.button1').click(function () {
         addBlock();
+        updateDisplay();
     });
 
-    console.log(cwFlexState);
-
+    //toggle the justification state of the items and update the display
+    $('.button2').click(function () {
+        toggleJustifyContent();
+        updateDisplay();
+        console.log(columnWrapperState);
+    });
 
 
 
