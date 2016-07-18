574e0a2aa73c8db8203dbb9788855877e043f061
Chris Bader <baederchris@gmail.com>
2016-07-18 10:13:02 +0200

    fixed page3

--- scripts/js_page3.js
+++ scripts/js_page3.js
@@ -8,70 +8,80 @@ $(document).ready(function(){
     var display = $('#column_wrapper4').css('display');
 
 
-    var updateDisplay = function () {
-        $('.code p').remove();
-        $('.code').prepend(
-            "<p>#column-wrapper:{justify-content:" + cwFlexState + "}</p>"
-        );
-    };
+
+    $(window).ready(function () {
+        updateDisplay();
+    });
+
+
 
     var toggleJustifyContent=function () {
 
             if ($('#column_wrapper4').hasClass('jcc')) {
-                updateDisplay();
+
                 $('#column_wrapper4').addClass('jcsa');
                 $('#column_wrapper4').removeClass('jcc');
                 $('.legende').addClass('stage2');
+                updateDisplay();
             }
 
             else if ($('.legende').hasClass('stage2')) {
 
                 $('#column_wrapper4').removeClass('jcsa');
                 $('#column_wrapper4').addClass('jcsb');
-                
-                updateDisplay();
-                
                 $('.legende').removeClass('stage2');
                 $('.legende').addClass('stage3');
+                updateDisplay();
             }
 
             //adn now for justify-content center
             else if ($('.legende').hasClass('stage3')) {
                 $('#column_wrapper4').removeClass('jcsb');
                 $('#column_wrapper4').addClass('jcc');
-                
-                updateDisplay();
-                
                 $('.legende').removeClass('stage3');
                 $('.legende').addClass('stage4');
+                updateDisplay();
             }
 
             else if ($('.legende').hasClass('stage4')) {
                 $('#column_wrapper4').removeClass('jcc');
                 $('#column_wrapper4').addClass('jcfe');
-                
-                updateDisplay();
-                
                 $('.legende').removeClass('stage4');
                 $('.legende').addClass('stage1');
+                updateDisplay();
             }
 
 
 
     };
+
+    var updateDisplay = function () {
+        $('.code p').remove();
+        $('.code').prepend(
+            "<p>#column-wrapper:{justify-content:" + getFlexState( $('#column_wrapper4') ) + "}</p>"
+        );
+    };
+
+
+
     var addBlock = function () {
         $('#column_wrapper4').prepend("<div class='box1'></div>");
     };
 
+    var getFlexState = function (element) {
+        return( $(element).css('justify-content') );
+    };
+
     $('.button2').click(function () {
         toggleJustifyContent();
-        updateDisplay();
     });
 
     //add a block to the container
     $('.button1').click(function () {
         addBlock();
     });
+
+    console.log(cwFlexState);
 
 
 
